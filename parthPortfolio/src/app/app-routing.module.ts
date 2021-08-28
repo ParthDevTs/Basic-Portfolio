1cb4c7861f9ada2f15b15faf584dd913ac7670e7
Parth Kathuria <parth.kathuria99@gmail.com>
2021-08-28 16:35:23 +0530

    added nav and profiles

--- parthPortfolio/src/app/app-routing.module.ts
+++ parthPortfolio/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'home', pathMatch:"full"},
+  { path: 'home', component: HomeComponent},
+  { path: 'home/profile', component: ProfileComponent},
+  { path: 'home/projects', component:ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
