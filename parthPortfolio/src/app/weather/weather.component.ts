796abd19fec0be809a139c929bc745cbc1ad7370
Parth Kathuria <parth.kathuria99@gmail.com>
2021-08-31 16:32:37 +0530

    added progressbar in weather

--- parthPortfolio/src/app/weather/weather.component.ts
+++ parthPortfolio/src/app/weather/weather.component.ts
@@ -12,12 +12,14 @@ export class WeatherComponent implements OnInit {
 
   ngOnInit(): void {}
   userCity: string;
-  
-  citydata:any = null;
+
+  citydata: any = null;
   url;
   errorMessage;
+  progressbar = false;
 
   getData() {
+    this.progressbar = true;
     this.getWeather();
   }
 
@@ -34,10 +36,10 @@ export class WeatherComponent implements OnInit {
 
     this.service.getWeather(citd).subscribe(
       (response) => {
-        city.temp = Math.floor(response.main.feels_like)-273;
-        city.feelsLike = Math.floor(response.main.feels_like)-273;
-        city.tempMin = Math.round(response.main.temp_min)-273;
-        city.tempMax = Math.round(response.main.temp_max)-273;
+        city.temp = Math.floor(response.main.feels_like) - 273;
+        city.feelsLike = Math.floor(response.main.feels_like) - 273;
+        city.tempMin = Math.round(response.main.temp_min) - 273;
+        city.tempMax = Math.round(response.main.temp_max) - 273;
         city.pressure = response.main.pressure;
         city.humidity = response.main.humidity;
         city.description = response.weather[0].description;
@@ -47,7 +49,10 @@ export class WeatherComponent implements OnInit {
           'http://openweathermap.org/img/w/' +
           response.weather[0].icon +
           '.png';
-        this.citydata=city
+        setTimeout(() => {
+          this.citydata = city;
+          this.progressbar = false;
+        }, 3000);
       },
       (error) => (this.errorMessage = 'Enter Valid City ')
     );
